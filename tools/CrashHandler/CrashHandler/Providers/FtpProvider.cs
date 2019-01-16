001596b369bda2fd80cdfecb9a8bbedf82f22135
Tomashu <tomash4my@gmail.com>
2019-01-16 09:07:06 +0100

    crash handler downloading reports from ftp wip

---
+++ tools/CrashHandler/CrashHandler/Providers/FtpProvider.cs
@@ -0,0 +1,54 @@
+﻿using CrashHandler.Properties;
+using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace CrashHandler.Providers
+{
+    public class FtpProvider
+    {
+        public List<string> GetNewReports()
+        {
+            List<string> newReports = new List<string>();
+            Log.Verbose("Getting new reports...");
+
+            try
+            {
+                FtpClient client = new FtpClient(Settings.Default.FtpUrl);
+                client.Credentials = new NetworkCredential(Settings.Default.FtpUser, Settings.Default.FtpPassword);
+                client.Connect();
+
+                List<string> items = client.GetListing("/crash_reports").Select(x => x.Name).ToList();
+                Directory.CreateDirectory("reports");
+
+                foreach (string item in items)
+                {
+                    string source = $"/crash_reports/{item}";
+                    string path = $"reports/{item}";
+                    if (File.Exists(path))
+                        Log.Warning($"File {item} already exists.");
+                    else if (!client.DownloadFile(path, source))
+                        Log.Error($"File {item} download failed.");
+                    else
+                    {
+                        client.DeleteFile(source);
+                        newReports.Add(item);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Log.Error($"Failed to get new reports: {e}");
+            }
+
+            if (newReports.Count > 0)
+                Log.Info($"Returned {newReports.Count} new reports.");
+            else
+                Log.Verbose("No new reports.");
+            return newReports;
+        }
+    }
+}
---
+++ tools/CrashHandler/CrashHandler/Providers/Log.cs
@@ -0,0 +1,10 @@
+﻿namespace CrashHandler.Providers
+{
+    public static class Log
+    {
+        public static void Verbose(string msg) { }
+        public static void Info(string msg) { }
+        public static void Warning(string msg) { }
+        public static void Error(string msg) { }
+    }
+}
